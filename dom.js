ce3b4fa40c3519b54cd00e41ffc48152c4fb0f96
dolphin23-jp <h062.dolphin23@gmail.com>
2026-04-26 22:00:36 +0900

    Delete dom.js

--- dom.js
+++
@@ -1,5 +0,0 @@
-export const $ = sel => document.querySelector(sel);
-export const $$ = sel => [...document.querySelectorAll(sel)];
-export function showScreen(id){ for(const s of $$('.screen'))s.classList.remove('active'); $(id).classList.add('active'); }
-export function modal(html){ const layer=$('#modalLayer'); layer.innerHTML=`<div class="modal glass">${html}</div>`; layer.classList.add('active'); }
-export function closeModal(){ const layer=$('#modalLayer'); layer.classList.remove('active'); layer.innerHTML=''; }
